f9dd0debac8b26077008edb7f309a4d86e97d357
Rohan Kamath <rohankamath1998@yahoo.com>
2020-09-24 03:18:46 +0530

    commit to master

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./App.scss";
-import movies from "./data.json";
-
-import { Card } from "./components/styles";
+import { Card } from "./components/Card";
+import movies from "./data";
 
 function App() {
   return (
     <main>
       {movies.map((movie) => (
-        <Card></Card>
+        <Card classes="mr" key={`${movie.id}`}>
+          <Card.Image src={movie.image} alt={movie.alt} />
+          <Card.Body>
+            <Card.Title>{movie.title}</Card.Title>
+            <Card.Text>{movie.desc}</Card.Text>
+            <Card.Text>{movie.rating}</Card.Text>
+            <Card.Button>{movie.ctaText}</Card.Button>
+          </Card.Body>
+        </Card>
       ))}
     </main>
   );
